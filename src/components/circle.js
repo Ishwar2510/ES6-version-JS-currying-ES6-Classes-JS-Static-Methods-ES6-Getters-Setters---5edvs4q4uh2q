bc807224d6d0a9b6f6a6231f7b3ac23c85ba7f3c
ishwar kumar <ishwar2510@gmail.com>
2022-07-14 10:24:47 +0530

    chnaged

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,18 @@
 // import using require
 
 // declare class
+class Circle extends Shape{
+    constructor(color,radius){
+        super(color);
+        this.radius=radius;
+    }
+    
+    calculateArea(){
+        return Math.PI*this.radius*this.radius;
+
+
+}
+}
 
 
 // export class using module.exports
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,20 @@
 // declare class
+class Shape{
 
+    constructor(color){
+        this.color=color;
+    }
+
+    drawShape(){
+
+    }
+
+    calculateArea(){
+
+    }
+
+    
+}
+// we will be using object destructing 
+module.exports={drawShape,calculateArea}
 // export class using module.exports
